0d8a2ecbfe734d88d2e7f93166829441c30c1303
imabhi01 <iamdev.abhishek@gmail.com>
2022-05-16 11:19:02 +0545

    bean array

---
+++ Hello/src/BeanArr.java
@@ -0,0 +1,50 @@
+// A Bean array is defined to be an array where for every value n in the array, there is also an element n-1 or n+1 in the
+// array.
+// For example, {2, 10, 9, 3} is a Bean array because
+// 2 = 3-1
+// 10 = 9+1
+// 3 = 2 + 1
+// 9 = 10 -1
+// Other Bean arrays include {2, 2, 3, 3, 3}, {1, 1, 1, 2, 1, 1} and {0, -1, 1}.
+// The array {3, 4, 5, 7} is not a Bean array because of the value 7 which requires that the array contains either the value 6
+// (7-1) or 8 (7+1) but neither of these values are in the array.
+// Write a function named isBean that returns 1 if its array argument is a Bean array. Otherwise it returns a 0.
+// If you are programming in Java or C#, the function signature is
+//  int isBean(int[ ] a)
+// If you are programming in C or C++, the function signature is
+//  int isBean(int a[ ], int len) where len is the number of elements in the array.
+
+public class BeanArr {
+    public static void main(String[] args){
+        System.out.println(beanAr(new int[] {2, 10, 9, 3}));
+        System.out.println(beanAr(new int[] {2, 2, 3, 3, 3}));
+        System.out.println(beanAr(new int[] {1, 1, 1, 2, 1, 1}));
+        System.out.println(beanAr(new int[] {0, -1, 1}));
+        System.out.println(beanAr(new int[] {3, 4, 5, 7}));
+        System.out.println(beanAr(new int[] {5, 8, 10, 1}));
+    }
+
+    public static int beanAr(int[] arr){
+        
+        boolean isBean = false;
+
+        for(int i = 0; i < arr.length; i++){
+            int num = arr[i];
+            for(int j = 0; j < arr.length; j++){
+                if((num+1) == arr[j] || (num-1) == arr[j]){
+                    isBean = true;
+                    break;
+                }else{
+                    isBean = false;
+                }
+            }
+            // System.out.println("arr[i] "+ num + " " + isBean);
+            if(isBean == false){
+                return 0;
+            }
+        }
+
+        return isBean == true ? 1 : 0;
+    }
+
+}
