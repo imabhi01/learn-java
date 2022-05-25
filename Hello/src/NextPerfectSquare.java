7ecd1c5d378998907f90526488ce9a0e6e560615
imabhi01 <iamdev.abhishek@gmail.com>
2022-05-25 13:53:35 +0545

    next perfect square

---
+++ Hello/src/NextPerfectSquare.java
@@ -0,0 +1,23 @@
+public class NextPerfectSquare{
+    public static void main(String[] args){
+        System.out.println(isSquare(6));
+        System.out.println(isSquare(36));
+        System.out.println(isSquare(0));
+        System.out.println(isSquare(-1));
+    }
+
+    public static int isSquare(int n){
+        
+        int square = 0;
+
+        if(n == 0) return 1;
+        
+        for(int i = 1; i < n;i++){
+            if(i*i > n){
+                square = i*i;
+                break;
+            }
+        }
+        return square;
+    }
+}
